d47ae6201b143ce2a2ca3f91753f9e7829d0071a
thanhquyendev <quyenthanh2208@gmail.com>
2021-08-17 21:04:40 +0700

    change status

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="modal flex">
     <div class="modal-content">
-      <p>Are you sure you want to exit? Your changes will not be saved?</p>
+      <p>Are you sure you want to exit? You changes will not be saved?</p>
       <div class="actions flex">
         <button @click="closeModal" class="purple">Return</button>
         <button @click="closeInvoice" class="red">Close</button>
