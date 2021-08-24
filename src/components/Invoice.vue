b563024e68773142dc002a67fd1a828b4e81d46b
thanhquyendev <quyenthanh2208@gmail.com>
2021-08-24 14:04:32 +0700

    change text

--- src/components/Invoice.vue
+++ src/components/Invoice.vue
@@ -15,7 +15,7 @@
           pending: invoice.invoicePending,
         }"
       >
-        <span v-if="invoice.invoicePaid">Paid Data</span>
+        <span v-if="invoice.invoicePaid">Paid Data 1</span>
         <span v-if="invoice.invoiceDraft">Draft Data</span>
         <span v-if="invoice.invoicePending">Pending Data</span>
       </div>
